71e4877e00015bccb73a392895bc16bc21b8dbe0
root <root@LAPTOP-7N4H458J>
2022-12-16 19:59:22 +0000

    Reorganiza estrutura de dados e cria procedimento de listagem de dados

--- main.c
+++ main.c
@@ -5,26 +5,13 @@
 
 #define TAM 5
 
-typedef struct registo
+typedef struct Atleta
 {
-    //Atleta
     char numeroAtleta[10]; // número do atleta
     char nome[50]; // nome do atleta
     char telefone[9]; // telefone do atleta
     int idade; // idade do atleta
-    //Atividade
-    char data[11]; // data da atividade
-    char atividade[50]; // atividade realizada pelo atleta
-    float tempo; // tempo de duração
-    float distancia; // distância percorrida
-    //Plano
-    char dataInicio[11]; // data de início da atividade
-    char horaInicio[5]; // hora de início da atividade
-    char dataFim[11]; // data de fim da atividade
-    char horaFim[5]; // hora de fim da atividade
-    // char atividade[50]; // atividade realizada pelo atleta
-    // float distancia; // distância percorrida
-} Dados;
+} Atleta;
 
 /*struct data
 {
@@ -33,31 +20,31 @@ typedef struct registo
     int ano;
 };*/
 
-// typedef struct registo
-// {
-//     char numeroAtleta[10]; // número do atleta
-//     char data[11]; // data da atividade
-//     char atividade[50]; // atividade realizada pelo atleta
-//     float tempo; // tempo de duração
-//     float distancia; // distância percorrida
-// } Atividade;
+typedef struct Atividade
+{
+    char numeroAtleta[10]; // número do atleta
+    char data[11]; // data da atividade
+    char atividade[50]; // atividade realizada pelo atleta
+    float tempo; // tempo de duração
+    float distancia; // distância percorrida
+} Atividade;
 
-// typedef struct registo
-// {
-//     char numeroAtleta[10]; // número do atleta
-//     char dataInicio[11]; // data de início da atividade
-//     char horaInicio[5]; // hora de início da atividade
-//     char dataFim[11]; // data de fim da atividade
-//     char horaFim[5]; // hora de fim da atividade
-//     char atividade[50]; // atividade realizada pelo atleta
-//     float distancia; // distância percorrida
-// } Plano;
+typedef struct Plano
+{
+    char numeroAtleta[10]; // número do atleta
+    char dataInicio[11]; // data de início da atividade
+    char horaInicio[5]; // hora de início da atividade
+    char dataFim[11]; // data de fim da atividade
+    char horaFim[5]; // hora de fim da atividade
+    char atividade[50]; // atividade realizada pelo atleta
+    float distancia; // distância percorrida
+} Plano;
 
 
 //Procedimento que lê a informação relativa a um ou mais atletas e armazena os dados
-Dados guardarAtleta(Dados Atleta[TAM])
+Atleta guardarAtleta(Atleta Atleta[TAM])
 {
-    int quantidade = 0, novoAtleta;
+    int novoAtleta;
 
     for (int i = 0; i < TAM; i++)
     {        
@@ -70,20 +57,19 @@ Dados guardarAtleta(Dados Atleta[TAM])
         printf("Digite a idade do atleta: \n");
         scanf("%d", &Atleta[i].idade);
 
-        quantidade++;
         printf("Inscrever novo atleta? (0-Não ou 1-Sim)\n");
         scanf("%d", &novoAtleta);
         if (novoAtleta == 0)
         {
             break;
         }    
-    }    
-    
+    }
 }
 
-/*Dados guardarAtividade(Dados Atividade[TAM], Dados Atleta[TAM])
+//Procedimento que lê a informação relativa a uma ou mais atividades realizadas e armazena os dados
+Atividade guardarAtividade(Atividade Atividade[TAM])
 {
-    int quantidade = 0, novaAtividade;
+    int novaAtividade;
     for (int i = 0; i < TAM; i++)
     {
         printf("Digite o número do atleta: \n");
@@ -97,8 +83,6 @@ Dados guardarAtleta(Dados Atleta[TAM])
         printf("Digite a distância percorrida durante a atividade: \n");
         scanf("%f", &Atividade[i].distancia);
 
-        
-        quantidade++;
         printf("Inscrever nova atividade? (0-Não ou 1-Sim)\n");
         scanf("%d", &novaAtividade);
         if (novaAtividade == 0)
@@ -107,12 +91,34 @@ Dados guardarAtleta(Dados Atleta[TAM])
         }        
     }    
     
-}*/
+}
+
+//Procedimento que lista os atletas cadastrados
+void listarAtleta(Atleta Atleta[TAM], int quantidadeAtletas)
+{
+    for (int i = 0; i < quantidadeAtletas; i++)
+    {
+        printf("Atleta %d\nNúmero do atleta: %s\nNome: %s\nTelefone: %s\nIdade: %d\n", i+1, Atleta[i].numeroAtleta, Atleta[i].nome, Atleta[i].telefone, Atleta[i].idade);       
+    }    
+}
+
+//Procedimento que lista as atividades realizadas
+void listarAtividade(Atividade Atividade[TAM], int quantidadeAtividades)
+{
+    for (int i = 0; i < quantidadeAtividades; i++)
+    {
+        printf("Atividade %d\nNúmero do atleta: %s\nData: %s\nAtividade: %s\nTempo: %.2fmin\nDistância: %.2fm\n", i+1, Atividade[i].numeroAtleta, Atividade[i].data, Atividade[i].atividade, Atividade[i].tempo, Atividade[i].distancia);       
+    }    
+}
 
 int main()
 {
-    Dados Atleta[TAM], Atividade[TAM];
+    int quantidade=1;
+    Atleta Atleta[TAM];
+    Atividade Atividade[TAM];
 
     guardarAtleta(Atleta);
-    //guardarAtividade(Atividade, Atleta);
+    guardarAtividade(Atividade);
+    listarAtleta(Atleta, quantidade);
+    listarAtividade(Atividade, quantidade);
 }
